01a66ee7e923a2cf3147f6e48e6565c8db8442f4
ProgrammingGeek02 <davideveriwhere@gmail.com>
2021-08-14 13:41:22 -0500

    webpack 5 complete

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
-console.log("weiben is great!")
+import component from './component';
+import image from './assets/images/doja.jpg';
+import './styles/style.css';
+import data from './assets/data/data.json';
+
+let array = [1,2,3];
+
+console.log(...array, 4,5,6)
+
+const img = document.createElement('img');
+img.src = image;
+document.body.appendChild(img);
+
+const nav = document.createElement("nav");
+
+let menu = "";
+
+data.links.forEach((el) => (menu += `<a href="${el[1]}">${el[0]}</a>`))
+
+nav.innerHTML = menu;
+nav.classList.add("menu")
+
+document.body.appendChild(component());
+document.body.appendChild(nav);
--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,43 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devServer: {
+        contentBase: './dist',
+        open: true
+    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'weibens awesome'
         })
-    ]
-}
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader'
+                ]
+            },
+            {
+                test: /\.(gif|jpg|png|jpeg|svg)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
